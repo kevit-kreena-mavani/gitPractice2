11f59ad4f6069c018a44d15e1c9015219b4ee2dd
kevit-kreena-mavani <kreena.mavani@kevit.io>
2022-06-21 10:03:59 +0530

    second feature updated

--- index.js
+++ index.js
@@ -43,3 +43,10 @@ var server = app.listen(8081, function () {
    
    console.log("Example app listening at http://%s:%s", host, port);
 })
+
+//second feature updated
+app.get('/', function (req, res) {
+    console.log("Got a GET request for the homepage");
+    const userCount = Math.ceil(Math.random() * 100);
+    res.send(`Hello GET. Users are: ${userCount}`);
+ })
